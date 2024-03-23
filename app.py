6e9634b3574504ca8f01c16124cc2a83d9b794c5
vahido9 <vayeeded@gmail.com>
2024-03-22 23:52:32 -0400

    Refactored GUI to remove globals

--- app.py
+++ app.py
@@ -5,117 +5,114 @@ from maze_solver import find_path
 from maze_classifier import MazeClassifier
 from PIL import Image, ImageTk
 
-window = tk.Tk()
-uploaded_image = None
-panel_uploaded = None
-panel_result = None
 
+class MazeSolverGui:
+    def __init__(self, window) -> None:
+        self.window = window
+        self.window.title("Maze Solver")
+        self.window.geometry("900x750")
+        self.uploaded_image = None
+        self.panel_uploaded = None
+        self.panel_result = None
+        self.mazeClassifier = MazeClassifier()
 
-def open_file_dialog():
-    file_path = filedialog.askopenfilename()
-    return file_path
+        self.upload_btn = tk.Button(
+            self.window,
+            text="Upload Image",
+            command=self.upload_image,
+        )
+        self.upload_btn.pack(pady=5)
 
+        self.generate_btn = tk.Button(self.window, text="Generate Maze")  # command= )
+        self.generate_btn.pack(pady=5)
 
-def upload_image():
-    global uploaded_image
-    global panel_uploaded
-    global panel_result
+        self.export_btn = tk.Button(
+            self.window, text="Export", command=self.export_image, state="disabled"
+        )
+        self.export_btn.pack(side="bottom", pady=5)
 
-    if panel_uploaded is not None:
-        panel_uploaded.destroy()
+        self.solve_btn = tk.Button(
+            self.window, text="Solve", command=self.solve_maze, state="disabled"
+        )
+        self.solve_btn.pack(side="bottom", pady=5)
 
-    if panel_result is not None:
-        panel_result.destroy()
+    def open_file_dialog(self):
+        file_path = filedialog.askopenfilename()
+        return file_path
 
-    image_path = open_file_dialog()
-    if image_path:
-        export_btn.config(state="disabled")
-        image = cv2.imread(image_path)
-        display_uploaded_image(image)
-        uploaded_image = image
-        solve_btn.config(state="normal")
-    else:
-        print("No image selected, exiting!")
-        return
+    def upload_image(self):
 
+        if self.panel_uploaded is not None:
+            self.panel_uploaded.destroy()
 
-def solve_maze():
-    global uploaded_image
-    global panel_result
+        if self.panel_result is not None:
+            self.panel_result.destroy()
 
-    if uploaded_image is None:
-        print("No image selected, can not solve!")
-        return
-    mazeClassifier = MazeClassifier()
-    try:
-        if mazeClassifier.is_maze(uploaded_image):
-            result_image = find_path(uploaded_image)
-
-            if panel_result is not None:
-                panel_result.destroy()
-
-            display_result_image(result_image)
-            solve_btn.config(state="disabled")
-            export_btn.config(state="normal")
-            export_btn["command"] = lambda: export_image(result_image)
+        image_path = self.open_file_dialog()
+        if image_path:
+            self.export_btn.config(state="disabled")
+            image = cv2.imread(image_path)
+            self.display_uploaded_image(image)
+            self.uploaded_image = image
+            self.solve_btn.config(state="normal")
         else:
-            print("The uploaded image does not look like a maze!")
+            print("No image selected, exiting!")
             return
-    except Exception as error:
-        print(error)
-        print("The uploaded maze could not be solved!")
-        return
 
+    def solve_maze(self):
 
-def display_uploaded_image(image):
-    global panel_uploaded
-    image = cv2.resize(image, (250, 250))
-    image = Image.fromarray(image)
-    image = ImageTk.PhotoImage(image)
-    panel_uploaded = tk.Label(window, image=image)
-    panel_uploaded.image = image
-    panel_uploaded.pack()
-
-
-def display_result_image(image):
-    global panel_result
-    image = cv2.resize(image, (250, 250))
-    image = Image.fromarray(image)
-    image = ImageTk.PhotoImage(image)
-    panel_result = tk.Label(window, image=image)
-    panel_result.image = image
-    panel_result.pack()
-
-
-def export_image(result_image):
-    file_path = filedialog.asksaveasfilename(
-        defaultextension=".png", initialfile="solved_maze.png"
-    )
-    if file_path:
+        if self.uploaded_image is None:
+            print("No image selected, can not solve!")
+            return
         try:
-            cv2.imwrite(file_path, cv2.cvtColor(result_image, cv2.COLOR_RGB2BGR))
-            print("Image saved successfully")
+            if self.mazeClassifier.is_maze(self.uploaded_image):
+                result_image = find_path(self.uploaded_image)
+
+                if self.panel_result is not None:
+                    self.panel_result.destroy()
+
+                self.display_result_image(result_image)
+                self.solve_btn.config(state="disabled")
+                self.export_btn.config(state="normal")
+                self.export_btn["command"] = lambda: self.export_image(result_image)
+            else:
+                print("The uploaded image does not look like a maze!")
+                return
         except Exception as error:
             print(error)
-            print("Failed to save image")
+            print("The uploaded maze could not be solved!")
+            return
+
+    def display_uploaded_image(self, image):
+        image = cv2.resize(image, (250, 250))
+        image = Image.fromarray(image)
+        image = ImageTk.PhotoImage(image)
+        self.panel_uploaded = tk.Label(window, image=image)
+        self.panel_uploaded.image = image
+        self.panel_uploaded.pack()
+
+    def display_result_image(self, image):
+        image = cv2.resize(image, (250, 250))
+        image = Image.fromarray(image)
+        image = ImageTk.PhotoImage(image)
+        self.panel_result = tk.Label(window, image=image)
+        self.panel_result.image = image
+        self.panel_result.pack()
+
+    def export_image(self, result_image):
+        file_path = filedialog.asksaveasfilename(
+            defaultextension=".png", initialfile="solved_maze.png"
+        )
+        if file_path:
+            try:
+                cv2.imwrite(file_path, cv2.cvtColor(result_image, cv2.COLOR_RGB2BGR))
+                print("Image saved successfully")
+            except Exception as error:
+                print(error)
+                print("Failed to save image")
 
 
 if __name__ == "__main__":
-    # main()
-    window.title("Maze Solver")
-    window.geometry("900x750")
-    upload_btn = tk.Button(window, text="Upload Image", command=upload_image)
-    upload_btn.pack(pady=5)
-
-    generate_btn = tk.Button(window, text="Generate Maze")  # command= )
-    generate_btn.pack(pady=5)
-
-    export_btn = tk.Button(
-        window, text="Export", command=export_image, state="disabled"
-    )
-    export_btn.pack(side="bottom", pady=5)
-
-    solve_btn = tk.Button(window, text="Solve", command=solve_maze, state="disabled")
-    solve_btn.pack(side="bottom", pady=5)
-
+    window = tk.Tk()
+    gui = MazeSolverGui(window)
     window.mainloop()
