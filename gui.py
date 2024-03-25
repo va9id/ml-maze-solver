63dc5ed47bc5bacbf755b7a82e17a82b5f79853a
Noah Hammoud <noah.hammoud01@gmail.com>
2024-03-25 14:18:14 -0400

    Updated GUI styles

--- gui.py
+++ gui.py
@@ -1,41 +1,66 @@
 import cv2
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import ttk
 from maze_solver import find_path
 from maze_classifier import MazeClassifier
 from PIL import Image, ImageTk
 from constants import IMAGE_SIZE
 
+class CustomButton(tk.Button):
+    def __init__(self, master=None, **kw):
+        tk.Button.__init__(self, master, **kw)
+        self.configure(
+            width=15,
+            borderwidth=0,
+            bg="#007FFF",
+            fg="white",
+            font=("Helvetica", 12, "bold")
+        )
 
 class MazeSolverGui:
     def __init__(self, window) -> None:
         self.window = window
+        self.window.resizable(False, False)
+        self.window.configure(background="#1B1B1B")
         self.window.title("Maze Solver")
-        self.window.geometry("900x750")
+        self.window.geometry("650x425")
         self.uploaded_image = None
         self.panel_uploaded = None
         self.panel_result = None
         self.mazeClassifier = MazeClassifier()
 
-        self.upload_btn = tk.Button(
+        self.upload_btn = CustomButton(
             self.window,
             text="Upload Image",
             command=self.upload_image,
         )
-        self.upload_btn.pack(pady=5)
+        self.upload_btn.grid(row=0, column=0, pady=20, padx=20, sticky="n")
 
-        self.generate_btn = tk.Button(self.window, text="Generate Maze")  # command= )
-        self.generate_btn.pack(pady=5)
+        self.generate_btn = CustomButton(
+            self.window, 
+            text="Generate Maze", 
+        )  # command= )
+        self.generate_btn.grid(row=0, column=1, pady=20, padx=20, sticky="n")
 
-        self.export_btn = tk.Button(
-            self.window, text="Export", command=self.export_image, state="disabled"
+        self.solve_btn = CustomButton(
+            self.window, 
+            text="Solve", 
+            command=self.solve_maze, 
+            state="disabled", 
         )
-        self.export_btn.pack(side="bottom", pady=5)
+        self.solve_btn.grid(row=0, column=2, pady=20, padx=20, sticky="n")
 
-        self.solve_btn = tk.Button(
-            self.window, text="Solve", command=self.solve_maze, state="disabled"
+        self.export_btn = CustomButton(
+            self.window, 
+            text="Export", 
+            command=self.export_image, 
+            state="disabled", 
         )
-        self.solve_btn.pack(side="bottom", pady=5)
+        self.export_btn.grid(row=0, column=3, pady=20, padx=20, sticky="n")
+
+        self.window.grid_rowconfigure(0, weight=1)
+        self.window.grid_columnconfigure((0, 1, 2, 3), weight=1)
 
     def open_file_dialog(self):
         '''
@@ -100,7 +125,7 @@ class MazeSolverGui:
         image = ImageTk.PhotoImage(image)
         self.panel_uploaded = tk.Label(self.window, image=image)
         self.panel_uploaded.image = image
-        self.panel_uploaded.pack()
+        self.panel_uploaded.grid(row=1, column=0, columnspan=2, pady=20)
 
     def display_result_image(self, image):
         '''
@@ -111,7 +136,7 @@ class MazeSolverGui:
         image = ImageTk.PhotoImage(image)
         self.panel_result = tk.Label(self.window, image=image)
         self.panel_result.image = image
-        self.panel_result.pack()
+        self.panel_result.grid(row=1, column=2, columnspan=2, pady=20) 
 
     def export_image(self, result_image):
         '''
